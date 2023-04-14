e709552c80a7c50c0f911050a611bb84040f8901
Filipe Bueno <filipe_bueno85@live.com>
2023-04-14 17:45:59 -0300

    Requisito 6 Crie o endpoint PUT /talker/:id

--- src/index.js
+++ src/index.js
@@ -69,6 +69,32 @@ async (req, res) => {
   return res.status(201).json(newTalker);
 });
 
+app.put('/talker/:id', 
+auth, 
+nameValidate, 
+ageValidate, 
+talkValidate, 
+watchedAtValidate, 
+rateValidate,
+async (req, res) => {
+  const talker = JSON.parse(await fs.readFile(talkerPath, 'utf-8'));
+  const { id } = req.params;
+  const { name, age, talk } = req.body;
+  const foundTalker = talker.find((tlk) => tlk.id === Number(id));
+  const newTalk = { id: Number(id), name, age, talk };
+  const newTalker = talker.map((tlk) => {
+    if (tlk.id === Number(id)) {
+      return newTalk;
+    }
+    return talk;
+  }); 
+  if (!foundTalker) {
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+  await fs.writeFile(talkerPath, JSON.stringify(newTalker));
+  return res.status(200).json(newTalk);
+});
+
 app.post('/login', emailValidate, passwordValidate, (req, res) => {
   const { email, password } = req.body;
   const loginArray = [email, password];
