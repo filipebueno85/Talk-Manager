36c2009322eb59b16acc9743e8ef247e699a4985
Filipe Bueno <filipe_bueno85@live.com>
2023-04-14 14:55:59 -0300

    Requisito 1 Crie o endpoint GET /talker

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const fs = require('fs').promises;
+const path = require('path');
+
+const talkerPath = path.resolve(__dirname, './talker.json');
 
 const app = express();
 app.use(express.json());
@@ -6,11 +10,21 @@ app.use(express.json());
 const HTTP_OK_STATUS = 200;
 const PORT = process.env.PORT || '3001';
 
+const readFile = async () => {
+  const response = await fs.readFile(talkerPath, 'utf-8')
+  return JSON.parse(response);
+};
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
+app.get('/talker', async (req, res) =>{
+  const talker = await readFile(talkerPath);
+  return res.status(200).json(talker);
+})
+
 app.listen(PORT, () => {
-  console.log('Online a');
+  console.log('Online');
 });
